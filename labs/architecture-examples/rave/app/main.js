d7a4403da3894e9e7ea6ada58efef9ae30bcfa00
Brian Cavalier <brian@hovercraftstudios.com>
2014-03-25 14:38:40 -0400

    Add rave-wire extension, and refactor main.js back to just an object literal wire spec. Update rave-when-debug.

--- labs/architecture-examples/rave/app/main.js
+++ labs/architecture-examples/rave/app/main.js
@@ -1,191 +1,185 @@
 /*global define */
-define(function(require) {
-	var wire = require('wire');
-	wire({
-		// Cujo uses OOCSS principles and thus separates theme (skin)
-		// from structure CSS.
+define({
+	// Cujo uses OOCSS principles and thus separates theme (skin)
+	// from structure CSS.
 //	theme: { module: 'css!theme/base.css' },
 
-		// The root node where all the views will be inserted
-		root: { $ref: 'dom!todoapp' },
+	// The root node where all the views will be inserted
+	root: { $ref: 'dom!todoapp' },
 
-		// Render and insert the create view
-		createView: {
-			render: {
-				template: { module: 'todomvc-RaveJS/app/create/template.html' },
-				replace: { module: 'todomvc-RaveJS/app/create/strings' }
-			},
-			insert: { first: 'root' }
+	// Render and insert the create view
+	createView: {
+		render: {
+			template: { module: 'todomvc-RaveJS/app/create/template.html' },
+			replace: { module: 'todomvc-RaveJS/app/create/strings' }
 		},
+		insert: { first: 'root' }
+	},
 
-		// Hook up the form to auto-reset whenever a new todo is added
-		createForm: {
-			element: { $ref: 'dom.first!form', at: 'createView' },
-			connect: { 'todos.onAdd': 'reset' }
-		},
+	// Hook up the form to auto-reset whenever a new todo is added
+	createForm: {
+		element: { $ref: 'dom.first!form', at: 'createView' },
+		connect: { 'todos.onAdd': 'reset' }
+	},
 
-		// Render and insert the list of todos, linking it to the
-		// data and mapping data fields to the DOM
-		listView: {
-			render: {
-				template: { module: 'todomvc-RaveJS/app/list/template.html' },
-				replace: { module: 'todomvc-RaveJS/app/list/strings' }//,
+	// Render and insert the list of todos, linking it to the
+	// data and mapping data fields to the DOM
+	listView: {
+		render: {
+			template: { module: 'todomvc-RaveJS/app/list/template.html' },
+			replace: { module: 'todomvc-RaveJS/app/list/strings' }//,
 //			css: { module: 'css!app/list/structure.css' }
-			},
-			insert: { after: 'createView' },
-			bind: {
-				to: { $ref: 'todos' },
-				comparator: 'dateCreated',
-				bindings: {
-					text: 'label, .edit',
-					complete: [
-						'.toggle',
-						{ attr: 'classList', handler: { module: 'todomvc-RaveJS/app/list/setCompletedClass' } }
-					]
-				}
-			}
 		},
+		insert: { after: 'createView' },
+		bind: {
+			to: { $ref: 'todos' },
+			comparator: 'dateCreated',
+			bindings: {
+				text: 'label, .edit',
+				complete: [
+					'.toggle',
+					{ attr: 'classList', handler: { module: 'todomvc-RaveJS/app/list/setCompletedClass' } }
+				]
+			}
+		}
+	},
 
-		// Render and insert the "controls" view--this has the todo count,
-		// filters, and clear completed button.
-		controlsView: {
-			render: {
-				template: { module: 'todomvc-RaveJS/app/controls/template.html' },
-				replace: { module: 'todomvc-RaveJS/app/controls/strings' }//,
+	// Render and insert the "controls" view--this has the todo count,
+	// filters, and clear completed button.
+	controlsView: {
+		render: {
+			template: { module: 'todomvc-RaveJS/app/controls/template.html' },
+			replace: { module: 'todomvc-RaveJS/app/controls/strings' }//,
 //			css: { module: 'css!app/controls/structure.css' }
-			},
-			insert: { after: 'listView' }
 		},
+		insert: { after: 'listView' }
+	},
 
-		// Render and insert the footer.  This is mainly static text, but
-		// is still fully internationalized.
-		footerView: {
-			render: {
-				template: { module: 'todomvc-RaveJS/app/footer/template.html' },
-				replace: { module: 'todomvc-RaveJS/app/footer/strings' }
-			},
-			insert: { after: 'root' }
+	// Render and insert the footer.  This is mainly static text, but
+	// is still fully internationalized.
+	footerView: {
+		render: {
+			template: { module: 'todomvc-RaveJS/app/footer/template.html' },
+			replace: { module: 'todomvc-RaveJS/app/footer/strings' }
 		},
+		insert: { after: 'root' }
+	},
 
-		// Create a localStorage adapter that will use the storage
-		// key 'todos-cujo' for storing todos.  This is also linked,
-		// creating a two-way linkage between the listView and the
-		// data storage.
-		todoStore: {
-			create: {
-				module: 'cola/adapter/LocalStorage',
-				args: 'todos-cujo'
-			},
-			bind: {
-				to: { $ref: 'todos' }
-			}
+	// Create a localStorage adapter that will use the storage
+	// key 'todos-cujo' for storing todos.  This is also linked,
+	// creating a two-way linkage between the listView and the
+	// data storage.
+	todoStore: {
+		create: {
+			module: 'cola/adapter/LocalStorage',
+			args: 'todos-cujo'
 		},
+		bind: {
+			to: { $ref: 'todos' }
+		}
+	},
 
-		todos: {
-			create: {
-				module: 'cola/Collection',
-				args: {
-					strategyOptions: {
-						validator: { module: 'todomvc-RaveJS/app/create/validateTodo' }
-					}
-				}
-			},
-			before: {
-				add: 'cleanTodo | generateMetadata',
-				update: 'cleanTodo'
-			}
-		},
-
-		// The main controller, which is acting more like a mediator in this
-		// application by reacting to events in multiple views.
-		// Typically, cujo-based apps will have several (or many) smaller
-		// view controllers. Since this is a relatively simple application,
-		// a single controller fits well.
-		todoController: {
-			create: 'todomvc-RaveJS/app/controller',
-			properties: {
-				todos: { $ref: 'todos' },
-
-				createTodo: { compose: 'form.getValues | todos.add' },
-				removeTodo: { compose: 'todos.remove' },
-				updateTodo: { compose: 'todos.update' },
-
-				querySelector: { $ref: 'dom.first!' },
-
-				masterCheckbox: { $ref: 'dom.first!#toggle-all', at: 'listView' },
-				countNode: { $ref: 'dom.first!.count', at: 'controlsView' },
-				remainingNodes: { $ref: 'dom.all!#todo-count strong', at: 'controlsView' }
-			},
-			on: {
-				createView: {
-					'submit:form': 'createTodo'
-				},
-				listView: {
-					'click:.destroy': 'removeTodo',
-					'change:.toggle': 'updateTodo',
-					'click:#toggle-all': 'toggleAll',
-					'dblclick:label': 'todos.edit',
-					'change,focusout:.edit': 'todos.submit',
-					'submit:form': 'todos.submit'
-				},
-				controlsView: {
-					'click:#clear-completed': 'removeCompleted'
-				}
-			},
-			connect: {
-				updateTotalCount: 'setTodosTotalState',
-				updateRemainingCount: 'setTodosRemainingState',
-				updateCompletedCount: 'setTodosCompletedState',
-				'todos.onChange': 'updateCount',
-				'todos.onEdit': 'todos.findNode | toggleEditingState.add | beginEditTodo',
-				'todos.onSubmit': 'todos.findNode | toggleEditingState.remove | todos.findItem | endEditTodo'
-			}
-		},
-
-		form: { module: 'cola/dom/form' },
-		cleanTodo: { module: 'todomvc-RaveJS/app/create/cleanTodo' },
-		generateMetadata: { module: 'todomvc-RaveJS/app/create/generateMetadata' },
-
-		toggleEditingState: {
-			create: {
-				module: 'wire/dom/transform/toggleClasses',
-				args: {
-					classes: 'editing'
+	todos: {
+		create: {
+			module: 'cola/Collection',
+			args: {
+				strategyOptions: {
+					validator: { module: 'todomvc-RaveJS/app/create/validateTodo' }
 				}
 			}
 		},
+		before: {
+			add: 'cleanTodo | generateMetadata',
+			update: 'cleanTodo'
+		}
+	},
 
-		setTodosTotalState: {
-			create: {
-				module: 'wire/dom/transform/cardinality',
-				args: { node: { $ref: 'root' }, prefix: 'todos' }
+	// The main controller, which is acting more like a mediator in this
+	// application by reacting to events in multiple views.
+	// Typically, cujo-based apps will have several (or many) smaller
+	// view controllers. Since this is a relatively simple application,
+	// a single controller fits well.
+	todoController: {
+		create: 'todomvc-RaveJS/app/controller',
+		properties: {
+			todos: { $ref: 'todos' },
+
+			createTodo: { compose: 'form.getValues | todos.add' },
+			removeTodo: { compose: 'todos.remove' },
+			updateTodo: { compose: 'todos.update' },
+
+			querySelector: { $ref: 'dom.first!' },
+
+			masterCheckbox: { $ref: 'dom.first!#toggle-all', at: 'listView' },
+			countNode: { $ref: 'dom.first!.count', at: 'controlsView' },
+			remainingNodes: { $ref: 'dom.all!#todo-count strong', at: 'controlsView' }
+		},
+		on: {
+			createView: {
+				'submit:form': 'createTodo'
+			},
+			listView: {
+				'click:.destroy': 'removeTodo',
+				'change:.toggle': 'updateTodo',
+				'click:#toggle-all': 'toggleAll',
+				'dblclick:label': 'todos.edit',
+				'change,focusout:.edit': 'todos.submit',
+				'submit:form': 'todos.submit'
+			},
+			controlsView: {
+				'click:#clear-completed': 'removeCompleted'
 			}
 		},
+		connect: {
+			updateTotalCount: 'setTodosTotalState',
+			updateRemainingCount: 'setTodosRemainingState',
+			updateCompletedCount: 'setTodosCompletedState',
+			'todos.onChange': 'updateCount',
+			'todos.onEdit': 'todos.findNode | toggleEditingState.add | beginEditTodo',
+			'todos.onSubmit': 'todos.findNode | toggleEditingState.remove | todos.findItem | endEditTodo'
+		}
+	},
 
-		setTodosRemainingState: {
-			create: {
-				module: 'wire/dom/transform/cardinality',
-				args: { node: { $ref: 'root' }, prefix: 'remaining' }
+	form: { module: 'cola/dom/form' },
+	cleanTodo: { module: 'todomvc-RaveJS/app/create/cleanTodo' },
+	generateMetadata: { module: 'todomvc-RaveJS/app/create/generateMetadata' },
+
+	toggleEditingState: {
+		create: {
+			module: 'wire/dom/transform/toggleClasses',
+			args: {
+				classes: 'editing'
 			}
-		},
+		}
+	},
 
-		setTodosCompletedState: {
-			create: {
-				module: 'wire/dom/transform/cardinality',
-				args: { node: { $ref: 'root' }, prefix: 'completed' }
-			}
-		},
+	setTodosTotalState: {
+		create: {
+			module: 'wire/dom/transform/cardinality',
+			args: { node: { $ref: 'root' }, prefix: 'todos' }
+		}
+	},
 
-		$plugins: [
-			'wire/debug',
-			'wire/dom',
-			'wire/dom/render',
-			'wire/on',
-			'wire/aop',
-			'wire/connect',
-			'cola'
-		]
-	}).done();
+	setTodosRemainingState: {
+		create: {
+			module: 'wire/dom/transform/cardinality',
+			args: { node: { $ref: 'root' }, prefix: 'remaining' }
+		}
+	},
 
-	return {};
+	setTodosCompletedState: {
+		create: {
+			module: 'wire/dom/transform/cardinality',
+			args: { node: { $ref: 'root' }, prefix: 'completed' }
+		}
+	},
+
+	$plugins: [
+		'wire/dom',
+		'wire/dom/render',
+		'wire/on',
+		'wire/aop',
+		'wire/connect',
+		'cola'
+	]
 });
